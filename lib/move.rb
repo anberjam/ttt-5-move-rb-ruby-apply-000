a332fc54ef2a06a98cf0ce444008b1dd482c520e
Anber Jamshed <anberjam@yahoo.com>
2020-08-01 21:26:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,12 +14,12 @@ def input_to_index(input)
    return a - 1
   end
 
-def move (board, input_to_index(input), char="X")
+def move (board, index, char="X")
   
   def update_array_at_with(array, index, value)
     array[index]=value
   end
-  update_array_at_with(board, input_to_index(input), char="X")
+  update_array_at_with(board, index, char="X")
   return board
 end
 
